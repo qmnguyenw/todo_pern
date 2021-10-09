1b11b16b460d76c6523e9e31d2274e9eccfecd55
QuangNguyen <qmnguyenw@gmail.com>
2021-10-09 17:27:32 +0700

    create db & connect db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db')
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
+
+// ROUTES //
+
+// get all todo
+
+// create a todo
+
+// get a todo
+
+// update a todo
+
+// delete a todo
+
 
 app.listen(5000, () => {
   console.log('server has started on port 5000');
