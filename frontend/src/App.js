ba42ac475b4f66015ce85e3e83512c384ec6eabb
QuangNguyen <qmnguyenw@gmail.com>
2021-10-11 00:10:50 +0700

    create frontend components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
-
-// components
 import InputTodo from './components/InputTodo';
+import ListTodo from './components/ListTodo';
 
 function App() {
   return (
     <Fragment>
       <div className='container'>
         <InputTodo />
+        <ListTodo />
       </div>
     </Fragment>
   );
